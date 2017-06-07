9c981c30683fa5e1bebc78c9a50ba393e9f34d93
EikaNN <octavi94@gmail.com>
2017-06-07 23:47:37 +0200

    Complete SVM mini-project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,30 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+# Slice the training set in order to speed up training
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+clf = SVC(kernel='rbf', C=10000)
+
+start = time()
+clf.fit(features_train, labels_train)
+print "Training time:", round(time()-start, 3), "s"
+
+start = time()
+pred = clf.predict(features_test)
+print "Testing time:", round(time()-start, 3), "s"
+
+accuracy = accuracy_score(labels_test, pred)
+print "The accuracy with C = 10000 is", accuracy
+
+for position in [10, 26, 50]:
+    print "prediction[", position, "] =", pred[position]
+
+print "There are", np.count_nonzero(pred), "emails predicted to be in the 1 class"
+
 #########################################################
-
-
