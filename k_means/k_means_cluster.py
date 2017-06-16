d27f9f708bad0b5211a0017ecc2ca63a3bd8b511
EikaNN <octavi94@gmail.com>
2017-06-16 20:40:59 +0200

    Complete k-means mini-project

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -4,17 +4,12 @@
     Skeleton code for k-means clustering mini-project.
 """
 
-
-
-
 import pickle
 import numpy
 import matplotlib.pyplot as plt
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-
-
 
 
 def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2"):
@@ -34,9 +29,6 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
     plt.xlabel(f1_name)
     plt.ylabel(f2_name)
     plt.savefig(name)
-    plt.show()
-
-
 
 ### load in the dict of dicts containing all the data on each person in the dataset
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
@@ -48,29 +40,48 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
-data = featureFormat(data_dict, features_list )
-poi, finance_features = targetFeatureSplit( data )
+features_list = [poi, feature_1, feature_2, feature_3]
+data = featureFormat(data_dict, features_list)
+poi, finance_features = targetFeatureSplit(data)
 
+feature = "exercised_stock_options"
+print "The minimum value taken by exercised_stock_options is", \
+    min([person[feature] for person in data_dict.values() if person[feature] != 'NaN'])
+
+print "The maximum value taken by exercised_stock_options is", \
+    max([person[feature] for person in data_dict.values() if person[feature] != 'NaN'])
+
+feature = "salary"
+print "The minimum value taken by salary is", \
+    min([person[feature] for person in data_dict.values() if person[feature] != 'NaN'])
+
+print "The maximum value taken by salary is", \
+    max([person[feature] for person in data_dict.values() if person[feature] != 'NaN'])
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
+for f1, f2, _ in finance_features:
     plt.scatter( f1, f2 )
-plt.show()
+plt.savefig("data.png")
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 
+from sklearn.cluster import KMeans
 
+kmeans = KMeans(n_clusters=2)
+kmeans.fit(data)
+
+pred = kmeans.predict(data)
 
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters_3_features.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
