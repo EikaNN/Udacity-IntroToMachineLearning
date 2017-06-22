d92b5abaae040dbb3c4920b4024197b0ab6eeb7c
EikaNN <octavi94@gmail.com>
2017-06-22 18:20:52 +0200

    Complete Evaluation Metrics mini-project

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -28,4 +28,22 @@ labels, features = targetFeatureSplit(data)
 
 ### your code goes here 
 
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import confusion_matrix, precision_score, recall_score
 
+np.random.seed(42)
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3)
+
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
+print "The number of predicted POIs in the test set is", np.count_nonzero(pred)
+print "The number of total people in the test set is", len(pred)
+
+print "The number of true positives is", confusion_matrix(labels_test, pred)[1, 1]
+print "The precision of the test data is", precision_score(labels_test, pred)
+print "The recall of the test data is", recall_score(labels_test, pred)
